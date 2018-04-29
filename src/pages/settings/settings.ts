f6be15eb1a9a02aa739236ff537057f2593b746d
Ryan Gordon <ryangordon210@gmail.com>
2018-04-29 18:50:54 +0100

    Added the same list of themes to settings page.

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+
+//App State class can be accessed from within  a page too 
+
+import {AppState} from '../../app/app.global';
+
 /**
  * Generated class for the SettingsPage page.
  *
@@ -15,11 +20,27 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SettingsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  pages: Array<{title: string, theme: string,color: string}>;
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+  private global:AppState) {
+    // used for an example of ngFor and navigation
+    this.pages = [
+      { title: 'Default Red Theme', theme: 'theme-red', color:'assets/imgs/FF0000.png' },
+      { title: 'Noir Theme', theme: 'theme-noir', color:'assets/imgs/333333.png' }
+    ];
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SettingsPage');
+  }
+
+
+
+  changeTheme(theme:any){
+    console.log("Now Changing theme to "+ theme);
+    this.global.set('theme', theme);
   }
 
 }
